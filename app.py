a01d0df321a00f3b2b56e3f30514e7d5dc9a0521
Tomas Zubiri <me@tomaszubiri.com>
2019-10-21 19:57:57 -0300

    employee list api endpoint supports limit argument and default. Test cases: /employees?limit=3 and /employees?arg=4&limit=3

--- app.py
+++ app.py
@@ -8,11 +8,16 @@ def employees():
   #TODO: check status code
   #TODO: Choose best proxying mechanism .text might contain unicode magic.
   #TODO: Set header as text/json
-  return requests.get(API_URL_ROOT + '/employees').text
+
+  limit = flask.request.args.get('limit')
+  if( limit == None ):
+    limit = 100
+
+  return requests.get(f'{API_URL_ROOT}/employees?limit={limit}').text
 
 @app.route('/employees/<int:employee_id>')
 def employee(employee_id):
-  return requests.get(API_URL_ROOT + '/employees?id=' + str(employee_id)).text
+  return requests.get(f'{API_URL_ROOT}/employees?id={employee_id}').text
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
