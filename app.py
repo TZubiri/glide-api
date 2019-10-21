1805265639d4cfbc1f570a239dbec80a5a0d347e
Tomas Zubiri <me@tomaszubiri.com>
2019-10-21 19:35:39 -0300

    /employees endpoint done

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import flask
-
+import requests
 app = flask.Flask('glide')
 API_URL_ROOT = 'https://rfy56yfcwk.execute-api.us-west-1.amazonaws.com/bigcorp'
 
-@app.route("/")
+@app.route("/employees")
 def hello():
-  a = 2+2
-  return "Hello World!"
+  #TODO: check status code
+  #TODO: Choose best proxying mechanism .text might contain unicode magic.
+  #TODO: Set header as text/json
+  return requests.get(API_URL_ROOT + '/employees').text
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
