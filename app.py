d324902f0dc5ff185d9de4ffaa2ec52539450ede
Tomas Zubiri <me@tomaszubiri.com>
2019-10-22 00:50:08 -0300

    depth-1 expansion working for single endpoint.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import flask
 import requests
 import json
+import typing
+
+
 app = flask.Flask('glide')
 API_URL_ROOT = 'https://rfy56yfcwk.execute-api.us-west-1.amazonaws.com/bigcorp'
 cache = {}
@@ -18,7 +21,7 @@ def employees():
     limit = '1000'
   #TODO: What happens on negative inputs?
 
-  if offset:= flask.request.args.get('offset'):
+  if offset := flask.request.args.get('offset'):
     offset_url_part = '&offset=' + offset
   else:
     offset_url_part = ''
@@ -39,10 +42,24 @@ def departments():
   return json.dumps(departments_json()[offset:offset+limit]),\
          200, {'content-type': 'application/json'}
 
+@app.route('/departments/<int:department_id>')
+def parse_department(department_id):
+  return expand([department(department_id-1)],flask.request.args.getlist('expand'))[0],\
+         200, {'content-type': 'application/json'}
+
+def department(department_id):
+  return departments_json()[department_id]
+
+
 @app.route('/offices')
 def offices():
   limit,offset = parse_args(flask.request.args)
   return json.dumps(offices_json()[offset:offset+limit]),\
+         200, {'content-type': 'application/json'}
+
+@app.route('/offices/<int:office_id>')
+def office(office_id):
+  return offices_json()[office_id+1],\
          200, {'content-type': 'application/json'}
 
 def parse_args(args):
@@ -53,6 +70,7 @@ def parse_args(args):
   limit = min(1000,limit)
 
   return limit,offset
+
 def load_file_from_memory_or_cache(file: str,force_read:bool = False):
   global cache
   if file not in cache:
@@ -62,13 +80,39 @@ def load_file_from_memory_or_cache(file: str,force_read:bool = False):
 
 def offices_json():
   return load_file_from_memory_or_cache('offices.json')
-def departments_json():
+def departments_json() -> typing.List[typing.Dict]:
   return load_file_from_memory_or_cache('departments.json')
 
 @app.route('/reload_data/4jz06v155')
 def read_json_files():
   load_file_from_memory_or_cache('offices.json',force_read=True)
   load_file_from_memory_or_cache('departments.json',force_read =True)
+
+def get_object_by_key_and_id(key,id):
+  key_to_func = {}
+  key_to_func["manager"] = employee;
+  key_to_func["office"] = office;
+  key_to_func["department"] = department;
+  key_to_func["superdepartment"] = department;
+
+  return key_to_func[key](id)
+
+def expand(objects_to_expand: typing.List,list_of_list_of_keys_to_expand):
+
+  for object_to_expand in objects_to_expand:
+    for list_of_keys_to_expand in list_of_list_of_keys_to_expand:
+      for key_to_expand in list_of_keys_to_expand.split('.'):
+
+      #TODO: Check if field exists.
+      #TODO: Check if field is an id.
+        if key_to_expand in object_to_expand:
+          id_to_expand = object_to_expand[key_to_expand]
+          if id_to_expand is None:
+            continue
+          object_to_expand[key_to_expand] = get_object_by_key_and_id(key_to_expand,id_to_expand)
+          object_to_expand = object_to_expand[key_to_expand]
+
+  return objects_to_expand
 
 
 #TODO: Serve app through apache or nginx
