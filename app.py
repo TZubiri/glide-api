61e7a409433891069c31024c9b0931c61612391b
Tomas Zubiri <me@tomaszubiri.com>
2019-10-21 21:12:46 -0300

    added caching mechanism along with user/admin reload for loading jsons.

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import flask
 import requests
 app = flask.Flask('glide')
 API_URL_ROOT = 'https://rfy56yfcwk.execute-api.us-west-1.amazonaws.com/bigcorp'
+cache = {}
+
 
 @app.route("/employees")
 def employees():
@@ -22,12 +24,43 @@ def employees():
   else:
     offset_url_part = ''
 
-  return requests.get(f'{API_URL_ROOT}/employees?limit={limit}{offset_url_part}').text,200,{'content-type':'application/json'}
+  return requests.get(f'{API_URL_ROOT}/employees?limit={limit}{offset_url_part}').text,\
+          200,{'content-type':'application/json'}
+
 
 @app.route('/employees/<int:employee_id>')
 def employee(employee_id):
-  return requests.get(f'{API_URL_ROOT}/employees?id={employee_id}').text,200,{'content-type':'application/json'}
+  return requests.get(f'{API_URL_ROOT}/employees?id={employee_id}').text,\
+         200, {'content-type':'application/json'}
+
+@app.route('/offices')
+def offices():
+    return offices_json(), 200, {'content-type':'application/json'}
+
+@app.route('/departments')
+def departments():
+    return departments_json(), 200, {'content-type': 'application/json'}
+
+
+def load_file_from_memory_or_cache(file: str,force_read:bool = False):
+  global cache
+  if file not in cache:
+    with open(file,'r') as f:
+      cache[file] = f.read()
+  return cache[file]
+
+def offices_json():
+  return load_file_from_memory_or_cache('offices_json')
+def departments_json():
+  return load_file_from_memory_or_cache('departments.json')
+
+@app.route('/reload_data/4jz06v155')
+def read_json_files():
+  load_file_from_memory_or_cache('offices.json',force_read=True)
+  load_file_from_memory_or_cache('departments.json',force_read =True)
+
 
 #TODO: Serve app through apache or nginx
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
+  read_json_files()
